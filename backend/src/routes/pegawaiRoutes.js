988027b37e559e4a1f3a0f2d9525ea9e1c6fe5fb
embuh75 <coolbanditz75@gmail.com>
2026-06-24 13:36:20 +0700

    Revert "Refactor pegawai management: update response format for delete action, improve file size validation for photo upload, and enhance form error messages"
    This reverts commit 898c7b6573e86a41da64e90e291b0ff89da570d3.

--- backend/src/routes/pegawaiRoutes.js
+++ backend/src/routes/pegawaiRoutes.js
@@ -11,45 +11,11 @@ const {
 const control = require("../controllers/pegawaiController");
 
 router.get("/", authenticate, hasPermission("pegawai:read"), control.getAll);
-router.get(
-  "/mapel",
-  authenticate,
-  hasPermission("pegawai:read"),
-  control.getMapel,
-);
-router.get(
-  "/jabatan",
-  authenticate,
-  hasPermission("pegawai:read"),
-  control.getJabatan,
-);
-router.get(
-  "/:id",
-  authenticate,
-  hasPermission("pegawai:read"),
-  control.getById,
-);
-router.post(
-  "/",
-  authenticate,
-  hasPermission("pegawai:create"),
-  uploadFoto.single("foto"),
-  createBodyValidator,
-  control.create,
-);
-router.put(
-  "/:id",
-  authenticate,
-  hasPermission("pegawai:update"),
-  uploadFoto.single("foto"),
-  updateBodyValidator,
-  control.update,
-);
-router.delete(
-  "/:id",
-  authenticate,
-  hasPermission("pegawai:delete"),
-  control.remove,
-);
+router.get("/mapel", authenticate, hasPermission("pegawai:delete"), control.getMapel);
+router.get("/jabatan", authenticate, hasPermission("pegawai:read"), control.getJabatan);
+router.get("/:id", authenticate, hasPermission("pegawai:read"), control.getById);
+router.post("/", authenticate, hasPermission("pegawai:create"), uploadFoto.single("foto"), createBodyValidator,  control.create);
+router.put("/:id", authenticate, hasPermission("pegawai:update"), uploadFoto.single("foto"), updateBodyValidator, control.update );
+router.delete("/:id", authenticate, hasPermission("pegawai:delete"), control.remove);
 
 module.exports = router;
